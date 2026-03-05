9280062c7f6857108c603b1eb666b53bdbd84213
Jorge Gabriel <105565673+jorgegabrielsl@users.noreply.github.com>
2026-03-04 21:33:24 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,14 @@
+import psycopg2
+import os
 
+
+def get_connection():
+    connection = psycopg2.connect(
+        database=os.getenv("DB_NAME"),
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        port=os.getenv("DB_PORT")
+    )
+
+    return connection
